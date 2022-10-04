d7f0a0041f5864d9787802f262709056556bda25
Anouk Van Bogaert <vb.anouk@gmail.com>
2022-10-04 03:36:21 +0000

    added POST

--- express_server.js
+++ express_server.js
@@ -4,22 +4,44 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
-const urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+app.use(express.urlencoded({extended: true}));
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+function generateRandomString() {
+  let text = ""
+  let possibilities = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  for (let i = 0; i < 6; i++) {
+    text += possibilities.charAt(Math.floor(Math.random() * possibilities.length))
+  }
+  return text
+}
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);   // urls_index needs to be an .ejs file in the views folder
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  let newKey = generateRandomString()
+  urlDatabase.newKey = req.body
+  res.send(newKey); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -27,9 +49,12 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n"); // getting hello, responding with hello world 
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n"); // getting hello, responding with hello world 
+// });
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
