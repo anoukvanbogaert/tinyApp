0e8b0c15cb0dd8d0c3124747f95496de47ea2dfe
Anouk Van Bogaert <vb.anouk@gmail.com>
2022-10-05 02:41:20 +0000

    added username and login/lgout

--- express_server.js
+++ express_server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
+const favicon = require('serve-favicon')
+const cookieParser = require('cookie-parser')
 const PORT = 8080;
-
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
+app.use('/public/images', express.static('public/images'));
+app.use(cookieParser())
 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.use(express.urlencoded({extended: true}));
 
 function generateRandomString() {
   let text = ""
@@ -27,6 +29,24 @@ function generateRandomString() {
 // app.get("/urls.json", (req, res) => {
 //   res.json(urlDatabase);
 // });
+
+app.post("/login", (req, res) => {
+  let username = req.body.username
+  console.log(username)
+  res.cookie('username', username)
+  res.redirect('/urls')
+});
+
+app.get("/login", (req, res) => {
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_index", templateVars);
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls')
+})
+
 app.post("/urls", (req, res) => {
   console.log("this is the req body", req.body); // Log the POST request body to the console
   let newKey = generateRandomString()
@@ -54,22 +74,25 @@ app.post("/urls/:id/update", (req, res) => {
 })
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  let myUsername = req.cookies["username"]
+  const templateVars = {urls: urlDatabase, username: myUsername};
   res.render("urls_index", templateVars);   // urls_index needs to be an .ejs file in the views folder
 });
 
 
 app.get("/u/:id", (req, res) => {
+  // const templateVars = {username: req.cookies["username"]};
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
 
 app.get("/urls/new", (req, res) => {
+  // const templateVars = {username: req.cookies["username"]};
   res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
